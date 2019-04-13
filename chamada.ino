94238670a5dddad481e3d13dcb1b40a8573cedd1
kbcvcbk <49483225+kbcvcbk@users.noreply.github.com>
2019-04-13 16:18:16 -0300

    added some comments and added extra buttons

--- chamada.ino
+++ chamada.ino
@@ -1,59 +1,111 @@
+// variável que controla se o código de aluno já foi escrito
 bool initi = false;
 
-int codeSize = 2;
+// variáveis que controlam o código final que vai ser escrito na
+// serial p/ indicar qual o aluno e qual sua escolha
+int codeSize = 4;
 String code;
+String choice;
 
+// pinos, a-d são os butões de escolha, con é o botão de
+// confirmação e led é o led (duuuh)
 int a = 2;
 int b = 3;
-int con = 4;
-int led = 8;
+int c = 4;
+int d = 5;
+int con = 6;
+int led = 9;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(a, INPUT_PULLUP);
   pinMode(b, INPUT_PULLUP);
+  pinMode(c, INPUT_PULLUP);
+  pinMode(d, INPUT_PULLUP);
   pinMode(con, INPUT_PULLUP);
   pinMode(led, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (!initi) {
-    code = "";
-    digitalWrite(led, HIGH);
-    int c = 0;
-    while (c < codeSize) {
+  if (!initi) { // se o controle ainda não foi inicializado
+    code = "";  // limpe o código do aluno
+    digitalWrite(led, HIGH);  // acenda o led
+    int count = 0;
+    while (count < codeSize) {  // e rode o código abaixo até que
+      // o código de aluno inteiro tenha
+      // sido escrito.
       if (!digitalRead(a)) {
         digitalWrite(led, LOW);
         delay(200);
         digitalWrite(led, HIGH);
-        code = code + "0";
-        c++;
+        code = code + "00";
+        count++;
       }
       if (!digitalRead(b)) {
         digitalWrite(led, LOW);
         delay(200);
         digitalWrite(led, HIGH);
-        code = code + "1";
-        c++;
+        code = code + "01";
+        count++;
+      }
+      if (!digitalRead(c)) {
+        digitalWrite(led, LOW);
+        delay(200);
+        digitalWrite(led, HIGH);
+        code = code + "10";
+        count++;
+      }
+      if (!digitalRead(d)) {
+        digitalWrite(led, LOW);
+        delay(200);
+        digitalWrite(led, HIGH);
+        code = code + "11";
+        count++;
       }
     }
     initi = true;
   }
-  if (initi) {
+  if (initi) {  // caso o controle já tenha sido atualizado
+    // entre no modo de resposta à quiz
     digitalWrite(led, LOW);
     if (!digitalRead(a)) {
+      Serial.println("button a pressed!");
       digitalWrite(led, HIGH);
-      Serial.println(code+"0");
+      choice = "00";
       delay(200);
       digitalWrite(led, LOW);
-      }
-    if (!digitalRead(b)) {
-      digitalWrite(led, HIGH);
-      Serial.println(code+"1");
-      delay(200);
-      digitalWrite(led, LOW);
-      }
     }
+    if (!digitalRead(b)) {
+      Serial.println("button b pressed!");
+      digitalWrite(led, HIGH);
+      choice = "01";
+      delay(200);
+      digitalWrite(led, LOW);
+    }
+    if (!digitalRead(c)) {
+      Serial.println("button c pressed!");
+      digitalWrite(led, HIGH);
+      choice = "10";
+      delay(200);
+      digitalWrite(led, LOW);
+    }
+    if (!digitalRead(d)) {
+      Serial.println("button d pressed!");
+      digitalWrite(led, HIGH);
+      choice = "11";
+      delay(200);
+      digitalWrite(led, LOW);
+    }
+
+    if (!digitalRead(con)) {
+      digitalWrite(led, HIGH);
+      Serial.println(code + choice);
+      delay(100);
+      digitalWrite(led, LOW);
+      delay(100);
+      digitalWrite(led, HIGH);
+      delay(100);
+      digitalWrite(led, LOW);
+    }
+  }
 }
