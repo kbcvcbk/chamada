13ccb1d132648ed5d026ddf130a820779c1f6ed5
kbcvcbk <49483225+kbcvcbk@users.noreply.github.com>
2019-04-18 20:02:03 -0300

    further compartimentalized code

--- code_translate.py
+++ code_translate.py
@@ -1,6 +1,7 @@
 import serial as srl
 
 comm = str(11)
+codeSize = int()
 
 def configComm(porta):
     global comm
@@ -15,20 +16,29 @@ def getCode():
                 didntRead = False
                 return int(lineRead)
 
+def stripCode(num):
+    global codeSize
+    codeSize = num&7
+    print(codeSize)
+    return num >> 4
+
 def parseCode(num, debug=False):
     if debug: print(f"num = {num:b}")
     code = str()
-    codeSize = num&7
     if debug: print(f"code size = {codeSize}")
-    num = num >> 4
     if debug: print(f"num2parse = {num:b}")
 
     for i in range(codeSize):
         if i == 1: code += " "
         cur = (num >> (2*i))&3
-        if debug: print("i = {0}\nnum = {1:b}\nnum&3: {2}".format(i, (num >> 2*i), (num >> 2*i)&3))
+        if debug:
+            print("i = {0}\nnum = {1:b}\nnum&3: {2}".format(
+                i, (num >> 2*i), (num >> 2*i)&3))
         if   cur == 0: code += "A"
         elif cur == 1: code += "B"
         elif cur == 2: code += "C"
         elif cur == 3: code += "D"
     return code[::-1]
+
+def decode():
+    print(parseCode(stripCode(getCode())))
